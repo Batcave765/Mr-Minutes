22cfa4274bdb65ec243110845f12e8abc151316f
Batcave765 <msriram765@gmail.com>
2023-12-13 19:41:15 +0530

    Added a clock function

--- mr.minutes/src/App.js
+++ mr.minutes/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header";
-import Stopwatch from "./Components/Stopwatch";
+// import Stopwatch from "./Components/Stopwatch";
+import TimeNow from "./Components/TimeNow";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
-			<Stopwatch />
+			{/* <Stopwatch /> */}
+			<TimeNow />
 		</div>
 	);
 }
